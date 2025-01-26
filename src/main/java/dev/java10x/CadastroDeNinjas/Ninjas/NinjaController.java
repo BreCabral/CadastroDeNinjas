10acc433e8643ac747c7b0ecac484a668ae4f99a
Breno <breno.ara.cabral@gmail.com>
2025-01-26 10:34:36 -0300

    feat: add endpoints to `NinjaContrllre`

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,16 +1,39 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("ninja")
 public class NinjaController {
 
-    @GetMapping("/boasVindas")
+    @GetMapping
     public String boasVindas() {
         return "Bem Vindo";
+    }
+
+    @GetMapping("list")
+    public String listNinjas() {
+        return "Lista de Ninjas";
+    }
+
+    @GetMapping("find/id")
+    public String findNinjaById() {
+        return "Ninja Find";
+    }
+
+    @PostMapping("add")
+    public String addNinja() {
+        return "Ninja adicionado";
+    }
+
+    @PutMapping("update/id")
+    public String updateNinjaById() {
+        return "Ninja atualizado";
+    }
+
+    @DeleteMapping("delete/id")
+    public String deleteNinjaById() {
+        return "Ninja deletado";
     }
 
 }
